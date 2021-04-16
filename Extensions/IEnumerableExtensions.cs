22f0b626947bc91011bccca3622573a8c7644457
jeffersonrcgouveia <jeffersonrcgouveia@gmail.com>
2021-04-17 00:56:20 +0100

    Fixing a bug in the method IsEmpty() where it wasn't working when the list contains references for gameobjects that was destroyed.

--- Extensions/IEnumerableExtensions.cs
+++ Extensions/IEnumerableExtensions.cs
@@ -1,14 +1,20 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 public static class IEnumerableExtensions
 {
-	public static bool IsEmpty<T>(this IEnumerable<T> list)
-    {
-        foreach (T obj in list)
-        {
-            if (obj != null) return false;
-        }
+	/**
+	 * Check if a collection of gameobjects is empty even after its items were destroyed, but not removed from the
+	 * collection. This method doesn't work properly if the collection type (GameObject) is replaced by a generic type.
+	 */
+	public static bool IsEmpty(this IEnumerable<GameObject> list)
+	{
+		foreach (GameObject obj in list)
+		{
+			if (obj != null) return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
+
 }
